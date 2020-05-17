1b18c4223bc56caccdd3a493e11b7176a066a20e
Sudeep <sudeep.srivastava@outlook.com>
2020-05-17 03:02:19 +0100

    Added travis-aws integration file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ahm Bramhasmi!
+          Ahm Bramhasmi with Docker!
         </p>
       </header>
     </div>
